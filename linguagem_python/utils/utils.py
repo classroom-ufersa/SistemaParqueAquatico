4a4204e8730e2731f388cde05e6d17bd5c995008
letsticia <leti.morais48@gmail.com>
2024-03-25 20:35:52 -0300

    feat: added piscina module

--- linguagem_python/utils/utils.py
+++ linguagem_python/utils/utils.py
@@ -1,5 +1,6 @@
 import streamlit as st
-from cliente import Cliente, listar_clientes, remover_cliente, editar_cliente
+from cliente import Cliente, listar_clientes, remover_cliente, editar_cliente, buscar_cliente
+from piscina import Piscina, listar_piscinas, remover_piscina, adicionar_banhistas
 
 def main():
     """ Função principal que exibe o menu de opções para o usuário
@@ -12,21 +13,21 @@ def main():
     choice = st.sidebar.selectbox("Escolha uma opção", menu_options)
     
     if choice == "Adicionar piscina":
-        st.write("Adicionar piscina")
+        piscina = Piscina()
+        piscina.adiciona_piscina()
     elif choice == "Remover piscina":
-        pass
+        remover_piscina()
     elif choice == "Adicionar cliente":
         cliente = Cliente()
         cliente.adiciona_cliente()
     elif choice == "Listar clientes":
         listar_clientes()    
-    
     elif choice == "Remover cliente":
         remover_cliente()
     elif choice == "Editar informações do cliente":
         editar_cliente()
     elif choice == "Buscar cliente por nome":
-        pass
+        buscar_cliente()
     elif choice == "Listar piscinas e o número de clientes presentes":
-        pass
+        listar_piscinas()
         
