af0c9ff1c19255906954809e43e4e6e79a90a0ae
alexrbss <alexandre.rbss@gmail.com>
2024-04-08 17:01:04 -0300

    docs: added coments

--- linguagem_C/include/sistema.h
+++ linguagem_C/include/sistema.h
@@ -7,10 +7,23 @@
 #include <unistd.h>
 #include <ctype.h>
 
-/* Função que solicita ao usuário que aperte a tecla ENTER para dar continuidade a execução do programa */
+/**
+ * Aguarda o usuário pressionar a tecla ENTER para continuar a execução do programa.
+ *
+ * Esta função exibe uma mensagem para o usuário pressionar a tecla ENTER para continuar.
+ * Em seguida, consome qualquer caractere no buffer de entrada até encontrar um caractere de nova linha '\n',
+ * indicando que o usuário pressionou a tecla ENTER, e então consome o próprio caractere de nova linha.
+ */
 void pressiona_enter();
 
-/* Função que limpa o buffer */
+
+/**
+ * Limpa o buffer de entrada até encontrar um caractere de nova linha ou o final do arquivo.
+ *
+ * Esta função lê caracteres do buffer de entrada até encontrar um caractere de nova linha '\n' ou o final do arquivo (EOF).
+ * Ela descarta esses caracteres, removendo-os do buffer de entrada.
+ * Isso é útil para limpar o buffer após a leitura de entrada, evitando que caracteres indesejados afetem as próximas operações de entrada.
+ */
 void LimpaBuffer();
 
 #endif // SISTEMA_H
